3f958c4a3506eedf2251781a31d1ce573ceedc61
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-07-21 18:43:11 +0300

    feat: make risk engine asynchonous

--- src/risk_engine/main.py
+++ src/risk_engine/main.py
@@ -1,20 +1,27 @@
-from fastapi import FastAPI
+import asyncio
 import logging
+from concurrent.futures import ThreadPoolExecutor
 from services.kafka_service import get_kafka_consumer
 from services.risk_assessment_service import assign_risk
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+executor = ThreadPoolExecutor(max_workers=5)
+
+async def consume_message(consumer):
+    for message in consumer:
+        logger.info(message)
+        await asyncio.sleep(1)  # simulate I/O-bound work with asyncio.sleep
 
 def main():
-    logger.info("Application startup")
+    logger.info("Risk engine startup")
     try:
         consumer = get_kafka_consumer("payment_topic")
 
-        for message in consumer:
-            logger.info(message)
+        loop = asyncio.get_event_loop()
+
+        loop.run_until_complete(consume_message(consumer))
     except Exception as e:
         logger.error(f"Unexpected error while doing risk assessment: {str(e)}")
 
