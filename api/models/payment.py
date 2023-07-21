b47235973fe95cf8b29a5c17b80a2483a569e821
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-07-21 12:53:06 +0300

    chore: move payment model to a separate file

---
+++ api/models/payment.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+
+class Payment(BaseModel):
+    amount: float = Field(..., gt=0)
+    currency: str
+    userId: str = Field(..., alias='userId')
+    payeeId: str = Field(..., alias='payeeId')
+    paymentMethodId: str = Field(..., alias='paymentMethodId')
+
+    
