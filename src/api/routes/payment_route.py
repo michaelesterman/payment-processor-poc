31532acfad2840683bd764e2ad1075d5013955f0
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-07-22 18:11:29 +0300

    feat: simplify sending payments

--- src/api/routes/payment_route.py
+++ src/api/routes/payment_route.py
@@ -15,16 +15,13 @@ router = APIRouter()
 
 @router.post("/payment")
 async def process_payment(payment: Payment, api_key: str = Depends(get_api_key)):
-    try:
-        payment_id = str(uuid4())
-
+    try:        
         logger.info(
-            f"Processing payment: {payment.model_dump_json()}, Request ID: {payment_id}")
+            f"Processing payment: {payment.model_dump_json()}, Request ID: {payment.payment_id}")
+        
+        await send_payment_to_kafka(payment)
 
-        payment_dict = convert_payment_to_dict(payment, payment_id)
-        send_payment_to_kafka(payment_dict)
-
-        return JSONResponse(status_code=200, content=PaymentResponse(status=StatusEnum.processing, payment_id=payment_id).model_dump())
+        return JSONResponse(status_code=200, content=PaymentResponse(status=StatusEnum.processing, payment_id=payment.payment_id).model_dump())
     except Exception as e:
         logger.error(f"Unexpected error while processing payment: {str(e)}")
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=PaymentResponse(status=StatusEnum.failure, payment_id=payment_id, message="Unexpected error while processing payment").model_dump())
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=PaymentResponse(status=StatusEnum.failure, payment_id=payment.payment_id, message="Unexpected error while processing payment").model_dump())
