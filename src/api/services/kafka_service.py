b7699905a8556a1589cf0f5ca0af7d766d6ad46f
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-07-22 18:10:42 +0300

    feat: add retries

--- src/api/services/kafka_service.py
+++ src/api/services/kafka_service.py
@@ -1,16 +1,24 @@
 import json
 import os
+import time
 from kafka import KafkaProducer
+from kafka.errors import NoBrokersAvailable
 
 from models.payment import Payment
 
 async def get_kafka_producer():
-    try:
-        producer = KafkaProducer(bootstrap_servers=os.environ.get(
-            'KAFKA_BROKER'), value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-        return producer
-    except Exception as e:
-        raise Exception(f"Error while creating Kafka producer: {e}")
+    while True:
+        try:
+            producer = KafkaProducer(bootstrap_servers=os.environ.get(
+                'KAFKA_BROKER'), value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+            return producer
+        except NoBrokersAvailable:
+                wait_time = 1  # wait time in seconds
+                print(f"Kafka is not available, retrying in {wait_time} seconds.")
+                time.sleep(wait_time)
+                continue
+        except Exception as e:
+            raise Exception(f"Error while creating Kafka producer: {e}")
     
 async def send_payment_to_kafka(payment: Payment):
     try:
