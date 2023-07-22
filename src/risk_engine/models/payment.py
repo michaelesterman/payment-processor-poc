33271e579d5635f87066eb77e95da8e819eaffe2
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-07-22 16:50:54 +0300

    feat: add payment_id to the model

--- src/risk_engine/models/payment.py
+++ src/risk_engine/models/payment.py
@@ -7,11 +7,4 @@ class Payment(BaseModel):
     userId: str = Field(..., alias='userId')
     payeeId: str = Field(..., alias='payeeId')
     paymentMethodId: str = Field(..., alias='paymentMethodId')
-
-class PaymentAssessed(BaseModel):
-    amount: float = Field(..., gt=0)
-    currency: str
-    userId: str = Field(..., alias='userId')
-    payeeId: str = Field(..., alias='payeeId')
-    paymentMethodId: str = Field(..., alias='paymentMethodId')
-    risk: float = Field(..., gt=0)
+    payment_id: str = Field(..., alias='payment_id')
