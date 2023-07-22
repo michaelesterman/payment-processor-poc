9b0fb0969627a6a1c015fad43a223bb084dad997
Michael Esterman <50673486+michaelesterman@users.noreply.github.com>
2023-07-22 16:50:41 +0300

    feat: add risk assessement poc

--- src/risk_engine/main.py
+++ src/risk_engine/main.py
@@ -1,19 +1,51 @@
 import asyncio
+from enum import Enum
 import logging
 from concurrent.futures import ThreadPoolExecutor
+import random
 from services.kafka_service import get_kafka_consumer
-from services.risk_assessment_service import assign_risk
+from models.payment import Payment
+from pydantic import BaseModel, Field
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 executor = ThreadPoolExecutor(max_workers=5)
 
+class VerdictEnum(str, Enum):
+    ACCEPTED = "accepted"
+    DECLINED = "declined"
+
+class RiskAssessment(BaseModel):
+    payment_id: str
+    risk_level: float
+    verdict: VerdictEnum
+
+class PaymentAssessed(BaseModel):
+     payment_request: Payment
+     risk_assessment: RiskAssessment
+
+async def output_payment(payment):
+    print("")
+    
+async def produce_risk_assessment(payment: Payment):
+    rand_num = random.uniform(0, 1)
+    if rand_num <= 0.7:
+        return RiskAssessment(payment_id=payment.payment_id, risk_level=rand_num, verdict=VerdictEnum.ACCEPTED)
+    else:
+        return RiskAssessment(payment_id=payment.payment_id, risk_level=rand_num, verdict=VerdictEnum.DECLINED)
+
 async def consume_message(consumer):
     for message in consumer:
-        logger.info(message)
-        await asyncio.sleep(1)  # simulate I/O-bound work with asyncio.sleep
+        payment = Payment.model_validate(message.value)
+        logger.info(payment)
+        
+        risk_assessment = await produce_risk_assessment(payment)
+        payment_assessed = PaymentAssessed(payment_request=payment, risk_assessment=risk_assessment)
 
+        logger.info(payment_assessed.model_dump_json())
+
+        
 def main():
     logger.info("Risk engine startup")
     try:
--- src/risk_engine/services/risk_assessment_service.py
+++
@@ -1,8 +0,0 @@
-import random
-
-from models.payment import Payment, PaymentAssessed
-
-
-def assign_risk(payment: Payment) -> PaymentAssessed:
-    risk = random.uniform(0, 1)
-    return {**payment, 'risk': risk}
